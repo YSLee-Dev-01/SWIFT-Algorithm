6cb58d1a1cae409652844050de6aa14fd4ec2ea9
YoonSuLee <ysleedev01@gmail.com>
2023-10-27 17:53:24 +0900

    Study: Set.Contains() 비교 추가

--- 15. A-Speed.playground/Contents.swift
+++ 15. A-Speed.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// array.contains()와 firstIndex, filter, 딕셔너리, Set으로 형변환의 시간 차이
+// array.contains()와 firstIndex, filter, 딕셔너리, Set.Contains()의 시간 차이
 func arrayContains() -> TimeInterval {
     var temp: [Int] = []
     (0 ..< Int(100000)).map {
@@ -49,14 +49,14 @@ func dictionary() -> TimeInterval{
     return start.timeIntervalSinceNow
 }
 
-func arrayToSet() -> TimeInterval {
-    var temp: [Int] = []
+func setContains() -> TimeInterval {
+    var temp = Set<Int>()
     (0 ..< Int(100000)).map {
-        temp.append($0)
+        temp.insert($0)
     }
     
     let start = Date()
-    Array(temp[0 ..< 99999])
+    _ = temp.contains(99999)
     
     return start.timeIntervalSinceNow
 }
@@ -70,8 +70,8 @@ print(-arrayFirstIndex())
 print("배열, filter")
 print(-arrayFilter())
 
-print("딕셔너리")
+print("딕셔너리, 접근")
 print(-dictionary())
 
-print("Set")
-print(-arrayToSet())
+print("Set, contains")
+print(-setContains())
