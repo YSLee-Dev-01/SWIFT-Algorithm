1e0d1046d2edeb6cb215700dcdcfe290bb3db662
YSLee-Dev <ysleedev01@gmail.com>
2025-01-26 21:12:58 +0900

    Study: RecursiveCall 정의, 함수  추가 (꼬리재귀함수 추가, 복습)

--- 4. A-RecursiveCall.playground/Contents.swift
+++ 4. A-RecursiveCall.playground/Contents.swift
@@ -42,6 +42,35 @@ func factorialWithRecursiveCall(_ num: Int) -> Int {
     
     if num == 1 {return num} // factorial 값이 1이 된 경우 num 값을 리턴
     return factorialWithRecursiveCall(num - 1) * num
+    
+    /// num이 1이 되기 전까지 Stack에 함수를 쌓다가, num이 1이 되어 return 될 경우
+    /// 가장 마지막에 호출된 함수부터 차례차례 자신의 값을 반환하여 맨 처음에 호출된 함수까지 Stack에
+    /// 쌓인 모든 함수가 return 되는 것
+    /// - 상단의 return (factorialWithRecursiveCall(num - 1)로 결과 값이 return
+    /// -> 원래 자신을 호출했던 구문으로 돌아감
+}
+
+print(factorialWithRecursiveCall(4))
+
+/// 재귀함수는 탈출조건이 없으면 무한루프에 빠질 수 있기 때문에 탈출 조건을 무조건 정의 해야함
+/// 재귀함수는 반복문으로 가능한 것들을 효율적으로 구현하기 위해서 사용
+/// - 반복문으로도 동일한 처리가 가능함
+///
+/// 그렇기 때문에 시간 복잡도는 O(n)을 가짐
+/// 단, 공간복잡도는 재귀함수의 호출이 n번을 가질 수 있기 때문에 O(n)을 가짐 (for은 호출은 1번 뿐이기에 O(1))
+/// Stack에 함수를 여러번 쌓기 때문에 스택오버플로우가 발생할 수 도 있음
+/// - 이를 해결하기 위해 꼬리재귀 함수를 사용하기도 함
+///
+/// 꼬리재귀 함수
+/// 기존 재귀함수의 스택오버플로우를 방지하고, 메모리를 효율적으로 사용하기 위해서 사용
+/// 계산의 누적을 담는 파라미터를 만들어, 스택을 다시 return 하는 것이 아닌 파라미터만 리턴함
+
+/// 꼬리재귀 함수를 사용한 factorial
+func factorialWithRecursiveCall(_ num: Int, result: Int) -> Int {
+    if num == 1 {
+        return result
+    }
+    return factorialWithRecursiveCall(num - 1, result: num - 1 * result)
 }
 
 print(factorialWithRecursiveCall(4))
