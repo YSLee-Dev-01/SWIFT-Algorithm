960f73f433afe15c2c8b5554f2bfd8d0212098d0
YSLee-Dev <ysleedev01@gmail.com>
2025-03-03 18:11:48 +0900

    Study: Heap 정의 및 구현 추가 (복습)

--- 18. A-Heap.playground/Contents.swift
+++ 18. A-Heap.playground/Contents.swift
@@ -15,7 +15,128 @@ import UIKit
 /// - 완전이진트리는 왼쪽 Node부터 차례차례 채워지며, 마지막 Level이 아니면 자식을 가지고 있기 때문에,
 /// - 자식이 부모의 index를 확인할 때는 자신 index / 2,
 /// - 부모가 자식의 index를 확인할 때는 자신 index * 2 (오른쪽인 경우 + 1)을 하면 알 수 있음
-/// 
+///
+/// insert
+/// 1. 완전 이진 트리에 맞춰서 데이터를 append
+/// 2. 삽입된 데이터가 부모의 Node 보다 크/작을 때까지 Swap 작업 진행
+///
+/// pop
+/// - Heap에서 pop은 가장 작은/큰 값인 RootNode를 꺼내는 것과 동일
+/// 1. RootNode인 값을 복사 후 가장 마지막 데이터와 Swap
+/// 2. 이동된 마지막 데이터와 자식 Node 간의 데이터를 비교하며 큰거나 작을 때 까지 Swap 작업을 반복
+/// - 자식간의 Node를 비교할 때에는 왼쪽, 오른쪽 Node간의 데이터도 전부 비교해야함
+/// - 1. 자식 노드가 있는지 -> 정지
+/// - 2. 오른쪽 자식 노드가 있는지 -> 왼쪽 노드만 비교
+/// - 3. 왼쪽 자식 노드와 오른쪽 자식 노드가 본인보다 크거나 작은지 ->  더 크거나 작은쪽으로 이동
+/// - 4. 3번에 해당하지 않아 둘 중의 한 자식 노드만 큰경우 각각 비교
+/// - 5. 자식 노드가 자신에 비해 크거나 작지 않은지 -> 정지
+///
+/// Heap의 시간복잡도는 O(log n)으로 배열의 O(n)보다 빠름
+
+struct MaxHeap<T: Comparable> {
+    private var heap: [T] = []
+    
+    init(data: T) {
+        heap.append(data) // 0번을 index로 사용할 경우 부모/자식의 index를 개발할 때 0번만 따로 처리해야하기 때문에 기준 index를 1로 시작
+        heap.append(data)
+    }
+    
+    mutating func insert(_ data: T) {
+        heap.append(data)
+        if heap.count <= 2 {return}
+        
+        var childIndex = (heap.count - 1)
+        var parentIndex = (heap.count - 1) / 2
+        while parentIndex > 0, heap[parentIndex] <= data {
+            heap.swapAt(parentIndex, childIndex)
+            childIndex = parentIndex
+            parentIndex = parentIndex / 2
+        }
+    }
+    
+    mutating func pop() -> T? {
+        if heap.count == 1 {return nil}
+        let data = heap[1]
+        let lastData = heap[heap.count - 1]
+        heap.swapAt(1, heap.count - 1)
+        heap.removeLast()
+        
+        var index = 1
+        while index < heap.count {
+            var childIndex = index * 2
+            
+            if childIndex >= heap.count { // 자식이 없을 때
+                break
+            } else if childIndex + 1 >= heap.count { // 오른쪽 자식이 없을 때
+                if lastData <= heap[childIndex] {
+                    heap.swapAt(index, childIndex)
+                    index = childIndex
+                } else {
+                    break
+                }
+            } else if lastData <= heap[childIndex], lastData < heap[childIndex + 1] { // 두개의 자식 값이 본인 보다 클 때
+                if heap[childIndex] <= heap[childIndex + 1] {
+                    heap.swapAt(index, childIndex + 1)
+                    index = childIndex
+                } else {
+                    heap.swapAt(index, childIndex)
+                    index = childIndex
+                }
+            } else if lastData <= heap[childIndex] { // 왼쪽 자식이 더 클 때
+                heap.swapAt(index, childIndex)
+                index = childIndex
+            } else if lastData < heap[childIndex + 1] { // 오른쪽 자식이 더 클 때
+                heap.swapAt(index, childIndex + 1)
+                index = childIndex
+            } else {
+                break
+            }
+        }
+        
+        return data
+    }
+    
+    func showHeap() {
+        print(heap)
+    }
+}
+
+var maxHeap = MaxHeap<Int>(data: 50)
+maxHeap.insert(100)
+maxHeap.insert(30)
+maxHeap.insert(10)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+maxHeap.insert(1)
+maxHeap.showHeap()
+maxHeap.insert(2)
+maxHeap.showHeap()
+maxHeap.insert(3)
+maxHeap.showHeap()
+maxHeap.insert(3)
+maxHeap.showHeap()
+maxHeap.insert(4)
+maxHeap.showHeap()
+maxHeap.insert(4)
+maxHeap.showHeap()
+maxHeap.insert(5)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+print(maxHeap.pop() ?? -1)
+maxHeap.showHeap()
+
 
 // 이전 공부 기록
 //// 데이터의 최소값, 최대값을 빠르게 알아내고 싶을 때 사용하는 데이터 구조
